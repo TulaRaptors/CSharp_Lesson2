30229a803fc2d4966cf7b13ce0d49ead78a9546a
Sergey_Podgornov <psv_23@mail.ru>
2022-10-19 15:40:03 +0300

    Выяснить является ли номер дня недели выходным

---
+++ Ex001_Weekend/Program.cs
@@ -0,0 +1,23 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+using System;
+
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt16(Console.ReadLine());
+
+if (number >= 1 && number <= 7)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("ВЫХОДНОЙ ДЕНЬ!");
+    }
+    else
+    {
+        Console.WriteLine("Будний день =(");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует!");
+}
