d2a8965cfbcd8ee2c534cdc959ce3c27fc616c26
Sergey_Podgornov <psv_23@mail.ru>
2022-10-19 19:44:10 +0300

    Проверяем число на палиндром

---
+++ Ex004_Palindrome/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+using System;
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+if (number.Length == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("Число является палиндромом!");
+    }
+    else
+    {
+        Console.WriteLine("Число НЕ является палиндромом");
+    }
+}
+else 
+{
+    Console.WriteLine("Вы ввели не пятизначное число!");
+}
